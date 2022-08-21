1b0ea26fee3e284ce08c62799e5b57ea630baf6e
Cornelius Suermann <cornelius.suermann@gmail.com>
2022-08-21 15:06:39 +0200

    log entire axios error

--- handler.ts
+++ handler.ts
@@ -255,7 +255,10 @@ async function handleChangeReport(event: VshClientBackchannelEvent) {
     try {
       return await pushDoorbellPressEventToAlexa(userId, event)
     } catch (e) {
-      log.error('pushDoorbellPressEventToAlexa FAILED! %s', e.response.data)
+      log.error(
+        'pushDoorbellPressEventToAlexa FAILED! %s',
+        JSON.stringify(e.response.data)
+      )
       return false
     }
   }
