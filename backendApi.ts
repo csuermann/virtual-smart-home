e1235298576133e1ede0a1bc0d907b871dcbeda0
Cornelius Suermann <cornelius.suermann@gmail.com>
2022-09-19 08:24:03 +0200

    allow promotion codes at checkout

--- backendApi.ts
+++ backendApi.ts
@@ -444,6 +444,7 @@ app.get(
           quantity: 1,
         },
       ],
+      allow_promotion_codes: true,
       // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
       // the actual Session ID is returned in the query parameter when your customer
       // is redirected to the success page.
